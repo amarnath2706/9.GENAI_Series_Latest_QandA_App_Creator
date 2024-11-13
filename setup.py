0481267f8911b691da2a2eb2133efa0d325fe008
Amar <amarnath.arjsd@gmail.com>
2024-11-13 23:42:06 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Que and Ans Creator App",
+    version= '0.0.0',
+    author="Amar",
+    author_email="amarnath.arjsd@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
